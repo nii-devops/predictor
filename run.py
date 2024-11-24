f82389f67f0c8502c768e5b4ee911b884a2f8932
Nii Ako Adjei <niiakoadjei.devops@gmail.com>
2024-11-24 16:46:01 +0000

    change port to production deployment mode

--- run.py
+++ run.py
@@ -5,19 +5,20 @@ from app import create_app, db
 app = create_app()
 
 
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Ensure database tables are created
-    app.run(debug=True)
-
-
-
-
-
+# # RUNNING ON LOCALHOST
 # if __name__ == '__main__':
 #     with app.app_context():
-#         db.create_all()  # Create tables if they don't exist
-#     app.run(host='0.0.0.0', port=8080, debug=True)
+#         db.create_all()  # Ensure database tables are created
+#     app.run(debug=True)
+
+
+
+
+
+# PRODUCTION ENVIRONMENT
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create tables if they don't exist
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
